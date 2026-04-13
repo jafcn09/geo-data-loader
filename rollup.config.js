e99a495d94375f6d4fb0ddbc7a73d7cba5e83617
jafcn09 <jafcnepamace24@gmail.com>
2026-04-13 11:55:33 -0500

    prepararse

---
+++ rollup.config.js
@@ -0,0 +1,53 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import { terser } from 'rollup-plugin-terser';
+import dts from 'rollup-plugin-dts';
+import filesize from 'rollup-plugin-filesize';
+
+const production = !process.env.ROLLUP_WATCH;
+
+export default [
+  // Builds: UMD and ESM
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: 'dist/index.js',
+        format: 'umd',
+        name: 'GeoDataLoader',
+        sourcemap: true,
+        globals: {
+          leaflet: 'L'
+        }
+      },
+      {
+        file: 'dist/index.esm.js',
+        format: 'es',
+        sourcemap: true
+      }
+    ],
+    external: ['leaflet'],
+    plugins: [
+      resolve(),
+      commonjs(),
+      typescript({
+        tsconfig: './tsconfig.json',
+        declaration: false,
+        declarationMap: false
+      }),
+      production && terser(),
+      filesize()
+    ]
+  },
+  // Type definitions
+  {
+    input: 'src/index.ts',
+    output: {
+      file: 'dist/index.d.ts',
+      format: 'es'
+    },
+    external: ['leaflet'],
+    plugins: [dts()]
+  }
+];
